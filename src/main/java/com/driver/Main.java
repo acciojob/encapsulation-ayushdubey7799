a73e94269c40d1fb0b2b07bf6a8224e397f30b00
ayushdubey7799 <ayushdubey7799@gmail.com>
2022-12-19 12:39:25 +0530

    .

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        rw.setName("Ayush");
+        System.out.print(rw.getName());
+//        'name' has private access in 'com.driver.RWOnly'
+    }
 }
